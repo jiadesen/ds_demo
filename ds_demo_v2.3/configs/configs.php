f5dbc0d3887571d17541683e8cf4875d295fa7a3
Loveayu <jiadesen1993@163.com>
2016-11-25 00:18:31 +0800

    ds_demo_v2.3
    ds_demo_v2.3

---
+++ ds_demo_v2.3/configs/configs.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/html;charset=UTF-8');
+//DB数据库配置信息(定义常量)
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PWD", "");
+define("DB_DBNAME", "shop");
+define("DB_CHARSET", "utf8");
