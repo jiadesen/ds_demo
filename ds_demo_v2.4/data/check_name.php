647be8148fb1c45a4e30ac9850fe4df501d2ff05
Loveayu <jiadesen1993@163.com>
2016-11-27 14:48:09 +0800

    de_demo_v2.4
    de_demo_v2.4

--- ds_demo_v2.4/data/check_name.php
+++ ds_demo_v2.4/data/check_name.php
@@ -7,8 +7,8 @@ $sql = "SET NAMES UTF8";
 mysqli_query($conn, $sql);
 $sql = "SELECT * FROM shop_user WHERE username='$username'";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
-if ($row) {
+//$row = mysqli_fetch_assoc($result);
+if ($result) {
     echo "cunzai";
 } else {
     echo "bucunzai";
--- ds_demo_v2.4/js/sign_in.js
+++ ds_demo_v2.4/js/sign_in.js
@@ -30,7 +30,7 @@ $('#username').blur(function () {
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    // console.log('响应完成且成功');
+                    console.log('响应完成且成功');
                     doResponse(xhr);
                 } else {
                     console.log('响应完成但有问题');
@@ -40,17 +40,18 @@ $('#username').blur(function () {
         xhr.open('GET', 'data/check_name.php?username=' + text, true);
         xhr.send(null);
         function doResponse(xhr) {
-            // console.log('开始处理响应数据');
+            console.log('开始处理响应数据');
             if (xhr.responseText === 'cunzai') {
                 m.innerHTML = '该用户名已存在';
                 m.className = 'default error';
             } else if (xhr.responseText === 'bucunzai') {
-                console.log(this);
+                // console.log(this);
                 m.innerHTML = '用户名格式正确';
                 m.className = 'default success';
                 return result_username = true;
             } else {
-                alert('未知的响应数据');
+                console.log('未知的响应数据');
+                // alert('未知的响应数据');
             }
         }
     }
@@ -94,45 +95,73 @@ email.onblur = function () {
 };
 
 //功能点3：jquery实现异步的提交注册信息
-$('#bt-register').click(function () {
-    //先判断表单是否都返回true，然后才能向服务器提交
+// $('#bt-register').click(function () {
+//     //先判断表单是否都返回true，然后才能向服务器提交
+//     if (result_username && result_password && result_email) {
+//         //表单序列化，获得所有用户输入 ('username=xxx&password=xxx&email=xxx')
+//         var data = $('#register').serialize();
+//         console.log(data);
+//         //AJAX异步提交请求数据
+//         $.ajax({
+//             type: 'POST',
+//             url: 'data/user_register.php',
+//             data: data,
+//             success: function (result) {
+//                 // console.log('开始处理服务器端返回的注册结果');
+//                 // console.log(result);
+//                 if (result.msg == 'succ') {
+//                     alert('注册成功!');
+//                     location.href = 'login.html';
+//                 } else {
+//                     alert('注册失败!请重新注册')
+//                 }
+//             }
+//         });
+//     } else {
+//         alert('请按规则填写注册信息!');
+//     }
+// });
+
+//功能点3：原生AJAX实现异步的提交注册信息
+document.getElementById('bt-register').onclick = function () {
+    //添加一层判断逻辑，只有当表单验证都返回true时才能向服务器提交数据
     if (result_username && result_password && result_email) {
-        //表单序列化，获得所有用户输入 ('username=xxx&password=xxx&email=xxx')
-        var data = $('#register').serialize();
-        console.log(data);
-        //AJAX异步提交请求数据
-        $.ajax({
-            type: 'POST',
-            url: 'data/user_register.php',
-            data: data,
-            success: function (result) {
-                // console.log('开始处理服务器端返回的注册结果');
-                // console.log(result);
-                if (result.msg == "succ") {
-                    alert('注册成功!');
-                    location.href = 'login.html';
+        var n = username.value;
+        var p = password.value;
+        var e = email.value;
+        // var t = null;
+        // var time = null; //接收服务器时间
+        // var sign_in_time = null; //定义注册时间
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                // time = xhr.getResponseHeader('Date');
+                // sign_in_time = new Date(time);
+                // console.log(time);
+                // console.log(sign_in_time);
+                // t = sign_in_time.getFullYear() + "-" + (sign_in_time.getMonth() + 1) + "-" + sign_in_time.getDate() + "/" + sign_in_time.getHours() + ":" + sign_in_time.getMinutes() + ":" + sign_in_time.getSeconds();
+                // console.log(t);
+                if (xhr.status === 200) {
+                    // console.log('响应完成且成功');
+                    doResponse(xhr);
                 } else {
-                    alert('注册失败!请重新注册')
+                    console.log('响应完成但有问题');
                 }
             }
-        });
+        };
+        xhr.open('POST', 'data/user_register_ys_ajax.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(`username=${n}&password=${p}&email=${e}`);
+        function doResponse(xhr) {
+            // console.log('开始处理响应数据');
+            if (xhr.responseText === 'succ') {
+                alert('注册成功!');
+                // location.href = 'login.html';
+            } else {
+                alert('注册失败!');
+            }
+        }
     } else {
         alert('请按规则填写注册信息!');
     }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
