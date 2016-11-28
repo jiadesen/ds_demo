c8d3778836431cbe9ca62e0837a4bccee84e5a58
Loveayu <jiadesen1993@163.com>
2016-11-28 19:57:00 +0800

    ds_demo_v2.5
    ds_demo_v2.5

--- ds_demo_v2.5/data/shop.sql
+++ ds_demo_v2.5/data/shop.sql
@@ -8,7 +8,7 @@ USE shop;
 /*管理员表*/
 CREATE TABLE shop_admin (
   id       INT PRIMARY KEY AUTO_INCREMENT, #TINYINT ，字段类型，如果设置为UNSIGNED类型，只能存储从0到255的整数,不能用来储存负数。
-  username VARCHAR(16), #管理员登录名
+  username VARCHAR(16) BINARY, #管理员登录名
   password VARCHAR(50), #管理员登录密码
   email    VARCHAR(32), #邮箱
   supAdmin ENUM ("是", "否") DEFAULT "否" #最高级管理员标识，值为1时允许对管理员账户进行增、删、改操作，默认值为0
@@ -54,7 +54,7 @@ CREATE TABLE shop_pro (
 /*用户表*/
 CREATE TABLE shop_user (
   id       INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(20),
+  username VARCHAR(20) BINARY,
   password VARCHAR(32),
   email    VARCHAR(32)
 );
